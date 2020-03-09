162eb46d6f163c24164b595ab46c1e33a05da3ff
kljdkolaj <kljdkolaj@gmail.com>
2020-03-09 07:08:39 +0100

    Add project files.

---
+++ aspnetcorejwt/Controllers/AuthenticationController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using aspnetcorejwt.Data;
+using aspnetcorejwt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace aspnetcorejwt.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class AuthenticationController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager)
+        {
+           _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if(user!=null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var authClaims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SecureKey"));
+
+                var token = new JwtSecurityToken(
+                    issuer: "http://dotnetdetail.net",
+                    audience: "http://dotnetdetail.net",
+                    expires: DateTime.Now.AddDays(2),
+                    claims: authClaims,
+                    signingCredentials: new Microsoft.IdentityModel.Tokens.SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+       
+    }
+}
---
+++ aspnetcorejwt/Data/SeedDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcorejwt.Data
+{
+    public class SeedDb
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            context.Database.EnsureCreated();
+            if (!context.Users.Any())
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    Email = "klajdi29@gmail.com",
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = "klajdik"
+                };
+                userManager.CreateAsync(user, "klajdi@123");
+            }
+        }
+    }
+}
